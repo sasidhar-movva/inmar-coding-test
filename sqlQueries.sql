3d3536e09466460ec528d7cee4290c5c21e458c3
Sasidhar Movva <33686271+sasidhar-movva@users.noreply.github.com>
2021-10-29 10:37:25 +0000

    Update sqlQueries.sql

--- sqlQueries.sql
+++ sqlQueries.sql
@@ -2,4 +2,6 @@
 2. select p.name from product p inner join orderproduct op on op.productid = p.productid 
 inner join order o on o.orderid = op.orderid where  o.cretaedat > to_Date("11/1/2016");
 
-3.
+3.select sum(p.price) from csutomer c 
+inner join order o on o.customerid = c.customerid
+inner join product p on p.productid = 
